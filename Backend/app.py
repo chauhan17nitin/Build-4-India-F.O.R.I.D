646f45bad79c537fd27c79e75263571bd4fd5c2d
root <root@ip-172-31-7-26.ap-south-1.compute.internal>
2020-01-18 17:25:15 +0000

    backend

--- Backend/app.py
+++ Backend/app.py
@@ -8,6 +8,7 @@ CORS(app)
 import pymysql
 
 connection = pymysql.connect(
+#database-1.cxedn5oo3lwl.ap-south-1.rds.amazonaws.com
     host = 'database-1.cxedn5oo3lwl.ap-south-1.rds.amazonaws.com',
     port = int(3306),
     user = "admin",
@@ -16,35 +17,39 @@ connection = pymysql.connect(
     charset ='utf8mb4'
     )
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return_statement = "Hello World"
-    return return_statement
+#    content = request.json
+    content = request.json["hello"]
+    world = request.json["time"]
+    print(content)
+    return world
 
 ##function for add user
-@app.route('/add_user',methods=['POST'])
+@app.route('/add_user',methods=['POST', 'GET'])
 def add_user():
     # cursor = mysql.connection.cursor()
-    cursor = connection.cursor()
+	cursor = connection.cursor()
     # user_info = request.json['user_info']
 
-    user_name = request.json['user_name']
-    first_name = request.json['first_name']
-    last_name = request.json['last_name']
-    dob = request.json['dob']
-    password = request.json['password']
-    address = request.json['address']
-    city = request.json['city']
-    state = request.json['state']
-
-    query = "INSERT INTO user (user_name,first_name,last_name,dob,password,address,city,state) VALUES ( user_name, first_name, last_name, dob, password, address, city, state)"
+	user_name = request.json['user_name']
+	first_name = request.json['first_name']
+	last_name = request.json['last_name']
+	dob = request.json['dob']
+	password = request.json['password']
+	address = request.json['address']
+	city = request.json['city']
+	state = request.json['state']
+#	query = "INSERT INTO user (user_name,first_name,last_name,dob,password,address,city,state) VALUES ( {}, {}, {}, '2038-01-19 03:14:07', {}, {}, {}, {})".format(user_name, first_name, last_name, password, address, str(city), state)
+	query = "INSERT INTO user (user_name, first_name, last_name, dob, password, address, city, state) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+	values = [user_name, first_name, last_name, dob, password, address, city, state]
 	try:
-		cursor.execute(query) 
-		cursor.close()   
+		cursor.execute(query, values)
 		connection.commit()
+		cursor.close()
 		return jsonify({'code':200, 'message': 'user addition Successful'})
 	except Exception as e:
-    	cursor.close()
-    	return jsonify({'code':400, 'message': 'user addition Successful'})
+    		cursor.close()
+    		return jsonify({'code':400, 'message': str(e)})
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
