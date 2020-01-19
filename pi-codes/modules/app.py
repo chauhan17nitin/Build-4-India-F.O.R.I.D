51bb3e5d7c00b1b94d28659661289e6296f58733
SRIJAN SINGH <41985103+srijansingh53@users.noreply.github.com>
2020-01-19 16:41:09 +0530

    updated predict.py

--- pi-codes/modules/app.py
+++ pi-codes/modules/app.py
@@ -13,22 +13,26 @@ def server():
     @app.route("/up", methods=['POST','GET'])
     def up():
         print('up') 
-        return 's'  
+        return 'up'  
     
     @app.route("/down", methods=['POST','GET'])
     def down():
         print('down')
-        return 'ss'
+        return 'down'
         
     @app.route("/display")
     def index():
+        with open('send.json') as outfile:
+            j = json.load(outfile)
         full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'map_final.png')
         return render_template("index.html", user_image = full_filename)
-
+        
     @app.route('/start', methods=['POST','GET'])
     def jsonreq():
         global data
         data = request.get_json('msg')
+        with open('received.json', 'w') as outfile:
+            j = json.dump(data, outfile,indent=4)
         print (data)
         execfile('modules/start.py')
         return data['msg']
